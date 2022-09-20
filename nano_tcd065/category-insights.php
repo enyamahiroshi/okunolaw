66a6d436917a3d3f97f20cc3621e26536e33bf83
en.works <45923194+enyamahiroshi@users.noreply.github.com>
2022-09-20 09:24:51 +0900

    コラム一覧修正

--- nano_tcd065/category-insights.php
+++ nano_tcd065/category-insights.php
@@ -13,9 +13,9 @@ $args = array(
 <div class="l-contents l-contents--grid<?php if ( 'type2' === $dp_options['sidebar'] ) { echo '-rev'; } ?>">
   <div class="l-contents__inner l-inner">
     <?php get_template_part( 'template-parts/page-header' ); ?>
-    <div class="l-primary">
+    <div class="l-primary l-insight">
       <section class="p-blog">
-        <div class="p-blog__list">
+        <div class="p-blog__list list-insight">
           <?php
           if ( have_posts() ) :
             while ( have_posts() ) :
@@ -23,8 +23,8 @@ $args = array(
               $categories = get_the_category();
           ?>
           <article class="p-blog__list-item p-article01 item-insight" data-aos="custom-fade">
-            <a class="p-hover-effect--<?php echo esc_attr( $dp_options['hover_type'] ); ?>" href="<?php the_permalink(); ?>">
-              <?php /*
+            <a class="p-hover-effect--<?php echo esc_attr( $dp_options['hover_type'] ); ?> item-insight__link" href="<?php the_permalink(); ?>">
+              <?php /* アイキャッチ画像
               <div class="p-article01__img">
                 <?php
                 if ( has_post_thumbnail() ) {
@@ -36,19 +36,26 @@ $args = array(
               </div>
               */ ?>
               <h3 class="p-article01__title item-insight__title"><?php echo wp_trim_words( get_the_title(), 31, '...' ); ?></h3>
-
-            <p class="p-article01__excerpt item-insight__excerpt"><?php echo is_mobile() ? wp_trim_words( get_the_excerpt(), 55, '...' ) : wp_trim_words( get_the_excerpt(), 74, '...' ); ?></p>
-            <?php if ( $dp_options['show_category'] || $dp_options['show_date'] ) : ?>
-            <p class="p-article01__meta">
-              <?php if ( $dp_options['show_category'] ) : ?>
-              <a class="p-article01__cat p-cat p-cat--<?php echo esc_attr( $categories[0]->term_id ); ?>" href="<?php echo get_category_link( $categories[0]->term_id ); ?>"><?php echo esc_html( $categories[0]->name ); ?></a>
+              <?php /* before:220915 「コラムタイトル+記事抜粋」
+              <p class="p-article01__excerpt item-insight__excerpt"><?php echo is_mobile() ? wp_trim_words( get_the_excerpt(), 55, '...' ) : wp_trim_words( get_the_excerpt(), 74, '...' ); ?></p>
+              */ ?>
+              <?php // ACF: 執筆者の入力があれば表示
+              $cf_writer = get_field('cf_writer');
+              if($cf_writer) {
+                echo '<p class="p-article01__excerpt item-insight__excerpt"><span>執筆者</span>' . $cf_writer . '</p>';
+              }
+              ?>
+              <?php if ( $dp_options['show_category'] || $dp_options['show_date'] ) : ?>
+              <p class="p-article01__meta">
+                <?php if ( $dp_options['show_category'] ) : ?>
+                <a class="p-article01__cat p-cat p-cat--<?php echo esc_attr( $categories[0]->term_id ); ?>" href="<?php echo get_category_link( $categories[0]->term_id ); ?>"><?php echo esc_html( $categories[0]->name ); ?></a>
+                <?php endif; ?>
+                <?php if ( $dp_options['show_date'] ) : ?>
+                <time class="p-article01__date" datetime="<?php the_time( 'Y-n-d' ); ?>"><?php the_time( 'Y.n.d' ); ?></time>
+                <?php endif; ?>
+              </p>
               <?php endif; ?>
-              <?php if ( $dp_options['show_date'] ) : ?>
-              <time class="p-article01__date" datetime="<?php the_time( 'Y-n-d' ); ?>"><?php the_time( 'Y.n.d' ); ?></time>
-              <?php endif; ?>
-            </p>
-            <?php endif; ?>
-              </a>
+            </a>
           </article>
           <?php
             endwhile;
